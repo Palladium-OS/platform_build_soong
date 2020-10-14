f9c418ef4544e9010546fa8a00f6cb02e6fe7e1c
Pig <pig.priv@gmail.com>
2020-10-15 02:50:07 +0800

    soong: Drop palladium soong product variables
     * This reverts commits c59e356, ce8c7b4, f6807aa in preparation
       for using Soong config variables as per Google's docs.
    
    Change-Id: Ice01dcd8da10e3e2c38394acd9fe9a9ccb1d9784

--- scripts/microfactory.bash
+++ scripts/microfactory.bash
@@ -59,7 +59,7 @@ function soong_build_go
     BUILDDIR=$(getoutdir) \
       SRCDIR=${TOP} \
       BLUEPRINTDIR=${TOP}/build/blueprint \
-      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong -pkg-path palladium/soong=${TOP}/vendor/palladium/build/soong -pkg-path github.com/golang/protobuf=${TOP}/external/golang-protobuf" \
+      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong -pkg-path github.com/golang/protobuf=${TOP}/external/golang-protobuf" \
       build_go $@
 }
 
